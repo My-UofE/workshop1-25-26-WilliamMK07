b14a8ac07f4da10e48e279f2b02d5f6eb52ad957
WilliamMK07 <wmk208@exeter.ac.uk>
2026-01-16 10:19:28 +0000

    fix game 2

--- game_functions.py
+++ game_functions.py
@@ -8,10 +8,11 @@ def pick_value(poss_values):
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
     if next_val < current_val:
-        if user_input == 'l'
+        if user_input == 'l':
             return True
-    else:
-        return False
+    elif next_val > current_val:
+        if user_input == 'h':
+            return True
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
